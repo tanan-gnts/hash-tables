8c2afc66df58a2b900927738848230d18eacb983
hexaier10 <2022081@miu.edu.mn>
2025-10-07 12:40:18 +0800

    Updated hashtables

--- lib/hashtables.dart
+++ lib/hashtables.dart
@@ -1,3 +1,14 @@
-int calculate() {
-  return 6 * 7;
+import 'package:hashtables/binary_search.dart';
+import 'package:hashtables/linear_search.dart';
+
+void main() {
+  final myList = [42, 3, 10, 7, 28, 149, 0, 3, 25, 7, 1];
+  var foundIndex = linearSearch(myList, 28);
+  print(foundIndex); // Output: 4
+
+  myList.sort();
+  print(myList); // Output: [0, 1, 3, 3, 7, 7, 10, 25, 28, 42, 149]
+
+  foundIndex = binarySearch(myList, 1);
+  
 }
