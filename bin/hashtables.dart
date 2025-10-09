892c15fea82a9da178225b044146845077847128
hexaier10 <2022081@miu.edu.mn>
2025-10-09 11:45:25 +0800

    Added students to the list

--- bin/hashtables.dart
+++ bin/hashtables.dart
@@ -7,24 +7,23 @@ void main() {
   print(foundIndex); // Output: 4
 
  final studentList = List<Student?>.filled(3000000, null);
-//   Student(2024078, 'AZ1'),
-//   Student(2024077, 'AZ2'),
-//   Student(2021018, 'AD'),
-//   Student(2023139, 'BB'),
-//   Student(2025068, 'BS'),
-//   Student(2024008, 'DD'),
-//   Student(2024054, 'EB'),
-//   Student(2024081, 'ED2'),
-//   Student(2024111, 'ES2'),
-//   Student(2024025, 'MB'),
-//   Student(2024020, 'MC'),
-//   Student(2023002, 'NM'),
-//   Student(2022081, 'TG'),
-//   Student(2024085, 'TK'),
-//   Student(2023096, 'TA'),
-//   Student(2022007, 'UM'),
-//   Student(2024007, 'VG'),
-//  ];
+ studentList[2024078] = Student(2024078, 'AZ1');
+ studentList[2024077] = Student(2024077, 'AZ2');
+ studentList[2021018] = Student(2021018, 'AD');
+ studentList[2023139] = Student(2023139, 'BB');
+ studentList[2025068] = Student(2025068, 'BS');
+ studentList[2024008] = Student(2024008, 'DD');
+ studentList[2024054] = Student(2024054, 'EB');
+ studentList[2024081] = Student(2024081, 'ED2');
+ studentList[2024111] = Student(2024111, 'ES2');
+ studentList[2024025] = Student(2024025, 'MB');
+ studentList[2024020] = Student(2024020, 'MC');
+ studentList[2023002] = Student(2023002, 'NM');
+ studentList[2022081] = Student(2022081, 'TG');
+ studentList[2024085] = Student(2024085, 'TK');
+ studentList[2023096] = Student(2023096, 'TA');
+ studentList[2022007] = Student(2022007, 'UM');
+ studentList[2024007] = Student(2024007, 'VG');
 
  final studentName = studentSearch(studentList, 2024008);
  print(studentName);
